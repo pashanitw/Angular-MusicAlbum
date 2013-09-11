b9a1ae657288446e109f5341055e7b9099cf7915
sourcebits-shaik <shaik.gousepasha@sourcebits.com>
2013-09-11 08:38:11 -0700

    changed services

--- app/js/services.js
+++ app/js/services.js
@@ -2,7 +2,6 @@
 
 /* Services */
 
-
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('musicAlbum.services', []).
@@ -18,8 +17,14 @@ angular.module('musicAlbum.services', []).
 
     })
 .factory('album',function(credentials,$http,mapper,$q){
-     var url=   "http://api.ent.nokia.com/1.x/us/?category=artist&domain=music&client_id=5e1490bd4983b770cfde86f5d77f0a05";
-     var getAlbumData=function(result){
+
+       //var url=   "http://api.ent.nokia.com/1.x/us/creators/986039/products/?domain=music&client_id=5e1490bd4983b770cfde86f5d77f0a05";
+        var url=   "http://api.ent.nokia.com/1.x/us/products/6710223/sample/?domain=music&client_id=5e1490bd4983b770cfde86f5d77f0a05";
+
+        // var url=   "http://api.ent.nokia.com/1.x/us/?category=artist&domain=music&client_id=5e1490bd4983b770cfde86f5d77f0a05";
+       // var url=   "http://api.ent.nokia.com/1.x/us/?category=artist&domain=music&client_id=5e1490bd4983b770cfde86f5d77f0a05";
+
+        var getAlbumData=function(result){
          var deferred = $.Deferred();
          var options={
              url:url,
